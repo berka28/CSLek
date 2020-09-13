417f91a48e0336a5a72a6f5a29b466fd828bdb86
Mathias Bergquist <berka28@hotmail.com>
2020-09-13 14:23:17 +0200

    Test 4

---
+++ Random_test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Random_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int temp = random.Next(40);
+
+            if (temp >= 20)
+
+                Console.WriteLine($" Temperaturen är {temp} grader och har överskridit gränsnivån");
+
+            else
+
+                Console.WriteLine($"Temperaturen är {temp} grader och är inom gränsnivån");
+
+
+            Random r = new Random();
+            Console.WriteLine(r.Next());
+            Console.WriteLine(r.Next(100));
+            Console.WriteLine(r.Next(1, 7));
+
+
+        }
+
+        
+    }
+}
